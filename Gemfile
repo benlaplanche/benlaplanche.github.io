ac25228cf155e5bf8135668bf1cf13d26aa549f6
Ben Laplanche <ben@laplanche.co.uk>
2019-05-31 13:54:29 +0100

    removing old blog

--- Gemfile
+++
@@ -1,7 +0,0 @@
-# A sample Gemfile
-source "https://rubygems.org"
-
-gem "jekyll"
-gem "html-proofer"
-gem "rake"
-gem "kramdown"
--- lib/test.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative "validator"
-require "pry"
-
-file = "/Users/benlaplanche/Sites/blog/_posts/2014-11-23-testing.md"
-a = Validator::FrontMatter.new(file)
-
-puts a
